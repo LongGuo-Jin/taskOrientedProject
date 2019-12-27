3d669660a0d5e855505b86b4393628aa819a371e
longguo1130 <longguo1130@outlook.com>
2019-12-27 17:30:26 +0800

    - Add attachment Model class.

---
+++ app/Model/Attachment.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use App\Helper\Common;
+
+class attachment extends Model
+{
+    protected $table = "attachment";
+
+    protected $fillable = [
+        'ID', 'fileName', 'tmpFileName', 'extension', 'timestamp', 'personID', 'taskID'
+    ];
+
+    public function getAttachmentByCond($cond)
+    {
+        $qrBuilder = DB::table($this->table)
+            ->leftjoin("person", "attachment.personID", "=", "person.ID")
+            ->leftjoin("task", "attachment.taskID", "=", "task.ID")
+            ->select("{$this->table}.*", DB::raw("concat(person.nameFamily, ' ', person.nameFirst) as fullName"));
+
+        if (isset($cond["taskID"]) && $cond["taskID"])
+            $qrBuilder = $qrBuilder->where("attachment.taskID", "=", $cond["taskID"]);
+        if (isset($cond["personID"]) && $cond["personID"])
+            $qrBuilder = $qrBuilder->where("attachment.personID", "=", $cond["personID"]);
+
+        $ret = $qrBuilder->orderBy('id', 'asc')->get()->toArray();
+
+        return Common::stdClass2Array($ret);
+    }
+
+    public function addAttachment($Attachment)
+    {
+        $ret = DB::table($this->table)
+            ->insert($Attachment);
+
+        return $ret;
+    }
+}
